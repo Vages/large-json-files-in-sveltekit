bc562ef005af16970d3445356d84c0da29ac6346
Eirik Vågeskar <kode@cappelendamm.no>
2023-03-21 17:06:47 +0100

    Move stuff around so that it's at root level

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -1,4 +1,4 @@
-export const prerender = true;
+// export const prerender = true;
 
 import sykkeldata from './05.json';
 
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,26 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	export let data;
+</script>
+
+<h3>a)</h3>
+<p>
+	Lag et program som presenterer de tre mest brukte startlokasjonene og de tre minst brukte
+	startlokasjonene. Presentasjonen skal også vise antall turer fra disse startlokasjonene.
+</p>
+
+<table>
+	<thead>
+		<tr>
+			<th>Plass</th>
+			<th>Stasjon</th>
+			<th>Turer</th>
+		</tr>
+	</thead>
+	{#each data.toppstasjonerSortert.slice(0, 3) as [stasjon, turer], index}
+		<tr>
+			<td>{index + 1}</td>
+			<td>{stasjon}</td>
+			<td>{turer}</td>
+		</tr>
+	{/each}
+</table>
