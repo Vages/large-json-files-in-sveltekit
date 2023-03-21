7faa2544d210d710e0ef77ffb137b1435b3d8885
Eirik Vågeskar <kode@cappelendamm.no>
2023-03-21 17:06:48 +0100

    Finish task 12a

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,25 +2,50 @@
 	export let data;
 </script>
 
-<h3>a)</h3>
+<h1>Oppgave 12</h1>
+
+<h2>a)</h2>
 <p>
 	Lag et program som presenterer de tre mest brukte startlokasjonene og de tre minst brukte
 	startlokasjonene. Presentasjonen skal også vise antall turer fra disse startlokasjonene.
 </p>
 
-<table>
-	<thead>
-		<tr>
-			<th>Plass</th>
-			<th>Stasjon</th>
-			<th>Turer</th>
-		</tr>
-	</thead>
-	{#each data.toppstasjonerSortert.slice(0, 3) as [stasjon, turer], index}
-		<tr>
-			<td>{index + 1}</td>
-			<td>{stasjon}</td>
-			<td>{turer}</td>
-		</tr>
-	{/each}
-</table>
+<figure>
+	<figcaption>De <em>mest</em> brukte stasjonene</figcaption>
+	<table>
+		<thead>
+			<tr>
+				<th>Plass</th>
+				<th>Stasjon</th>
+				<th>Turer</th>
+			</tr>
+		</thead>
+		{#each data.toppstasjonerSortert.slice(0, 3) as [stasjon, turer], index}
+			<tr>
+				<td>{index + 1}</td>
+				<td>{stasjon}</td>
+				<td>{turer}</td>
+			</tr>
+		{/each}
+	</table>
+</figure>
+
+<figure>
+	<figcaption>De <em>minst</em> brukte stasjonene</figcaption>
+	<table>
+		<thead>
+			<tr>
+				<th>Plass</th>
+				<th>Stasjon</th>
+				<th>Turer</th>
+			</tr>
+		</thead>
+		{#each data.toppstasjonerSortert.slice(0, 3) as [stasjon, turer], index}
+			<tr>
+				<td>{index + 1}</td>
+				<td>{stasjon}</td>
+				<td>{turer}</td>
+			</tr>
+		{/each}
+	</table>
+</figure>
