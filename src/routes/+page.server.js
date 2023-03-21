cd6638f2ebd9f3bb7d19c0550bb3814b4a71f65b
Eirik Vågeskar <kode@cappelendamm.no>
2023-03-21 17:06:48 +0100

    Solve second task

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -2,15 +2,22 @@
 
 import sykkeldata from './05.json';
 
-let toppstasjoner = {};
+const toppstasjoner = sykkeldata.reduce((acc, stasjon) => {
+	const turtall = (acc[stasjon.start_station_name] || 0) + 1;
+	return { ...acc, [stasjon.start_station_name]: turtall };
+}, {});
 
-for (const stasjon of sykkeldata) {
-	toppstasjoner[stasjon.start_station_name] = (toppstasjoner[stasjon.start_station_name] || 0) + 1;
-}
+const tripsByDayOfWeek = sykkeldata
+	.map((trip) => {
+		return new Date(trip.started_at).getDay();
+	})
+	.reduce((acc, day) => {
+		return { ...acc, [day]: (acc[day] || 0) + 1 };
+	}, {});
 
 let toppstasjonerSortert = Object.entries(toppstasjoner).sort((a, b) => b[1] - a[1]);
 
 /** @type {import('./$types').PageServerLoad} */
 export async function load() {
-	return { toppstasjonerSortert };
+	return { toppstasjonerSortert, tripsByDayOfWeek };
 }
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,19 @@
 <script>
 	export let data;
+
+	$: maxTripsInOneDay = Math.max(...Object.values(data.tripsByDayOfWeek));
+
+	const nummerTilUkedag = new Map([
+		[1, 'Mandag'],
+		[2, 'Tirsdag'],
+		[3, 'Onsdag'],
+		[4, 'Torsdag'],
+		[5, 'Fredag'],
+		[6, 'Lørdag'],
+		[0, 'Søndag']
+	]);
+
+	console.log(nummerTilUkedag.entries());
 </script>
 
 <h1>Oppgave 12</h1>
@@ -49,3 +63,36 @@
 		{/each}
 	</table>
 </figure>
+
+<h2>b)</h2>
+<p>
+	Utvid programmet slik at det også presenter et passende diagram som viser totalt antall turer fra
+	alle startlokasjoner til sammen, per ukedag.
+</p>
+
+<table>
+	<thead>
+		<tr>
+			<th>Ukedag</th>
+			<th>Turer</th>
+		</tr>
+		{#each [...nummerTilUkedag.entries()] as [day, name]}
+			<tr>
+				<td>{name}</td>
+				<td style="width: 300px">
+					<div class="bar" style="width: {(data.tripsByDayOfWeek[day] * 100) / maxTripsInOneDay}%;">
+						{data.tripsByDayOfWeek[day]}
+					</div>
+				</td>
+			</tr>
+		{/each}
+	</thead>
+</table>
+
+<style>
+	.bar {
+		background-color: #4caf50;
+		color: white;
+		padding: 5px;
+	}
+</style>
