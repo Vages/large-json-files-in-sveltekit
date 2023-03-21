aec728ac94ad457f9605474ad0a38f0027605ebc
Eirik Vågeskar <kode@cappelendamm.no>
2023-03-21 17:06:48 +0100

    Move things into their own files

---
+++ src/routes/EnkeltStolpediagram.svelte
@@ -0,0 +1,36 @@
+<script>
+	/** @type {[string, number][]} */
+	export let data = [];
+
+	/** @type {[string, string]} */
+	export let overskrifter = ["stolpenavn", "verdi"];
+
+	const max = Math.max(...data.map(([, count]) => count));
+</script>
+
+<table>
+	<thead>
+		<tr>
+			<th>{overskrifter[0]}</th>
+			<th>{overskrifter[1]}</th>
+		</tr>
+		{#each data as [stolpenavn, verdi]}
+			<tr>
+				<td>{stolpenavn}</td>
+				<td style="width: 300px">
+					<div class="bar" style="width: {(verdi * 100) / max}%;">
+						{verdi}
+					</div>
+				</td>
+			</tr>
+		{/each}
+	</thead>
+</table>
+
+<style>
+	.bar {
+		background-color: #4caf50;
+		color: white;
+		padding: 5px;
+	}
+</style>
---
+++ src/routes/Oppgave12.svelte
@@ -0,0 +1,86 @@
+<script>
+	import EnkeltStolpediagram from './EnkeltStolpediagram.svelte';
+
+	/** @type {[string, number][]} */
+	export let turerPerStasjon;
+
+	/** @type {Record<string, number>} */
+	export let turerPerUkedag;
+
+	const nummerTilUkedag = new Map([
+		[1, 'Mandag'],
+		[2, 'Tirsdag'],
+		[3, 'Onsdag'],
+		[4, 'Torsdag'],
+		[5, 'Fredag'],
+		[6, 'Lørdag'],
+		[0, 'Søndag']
+	]);
+
+	$: ukedagtabell = [...nummerTilUkedag.entries()].map(([nummer, navn]) => {
+		return [navn, turerPerUkedag[nummer]];
+	});
+
+	$: stasjonerSortertSynkendeEtterAntallTurer = Object.entries(turerPerStasjon).sort(
+		(a, b) => b[1] - a[1]
+	);
+</script>
+
+<h1>Oppgave 12</h1>
+
+<h2>a)</h2>
+<p>
+	Lag et program som presenterer de tre mest brukte startlokasjonene og de tre minst brukte
+	startlokasjonene. Presentasjonen skal også vise antall turer fra disse startlokasjonene.
+</p>
+
+<figure>
+	<figcaption>De <em>mest</em> brukte stasjonene</figcaption>
+	<table>
+		<thead>
+			<tr>
+				<th>Plass</th>
+				<th>Stasjon</th>
+				<th>Turer</th>
+			</tr>
+		</thead>
+		{#each stasjonerSortertSynkendeEtterAntallTurer.slice(0, 3) as [stasjon, turer], index}
+			<tr>
+				<td>{index + 1}</td>
+				<td>{stasjon}</td>
+				<td>{turer}</td>
+			</tr>
+		{/each}
+	</table>
+</figure>
+
+<figure>
+	<figcaption>De <em>minst</em> brukte stasjonene</figcaption>
+	<table>
+		<thead>
+			<tr>
+				<th>Plass</th>
+				<th>Stasjon</th>
+				<th>Turer</th>
+			</tr>
+		</thead>
+		{#each stasjonerSortertSynkendeEtterAntallTurer.slice(-3).reverse() as [stasjon, turer], index}
+			<tr>
+				<td>{index + 1}</td>
+				<td>{stasjon}</td>
+				<td>{turer}</td>
+			</tr>
+		{/each}
+	</table>
+</figure>
+
+<h2>b)</h2>
+<p>
+	Utvid programmet slik at det også presenter et passende diagram som viser totalt antall turer fra
+	alle startlokasjoner til sammen, per ukedag.
+</p>
+
+<figure>
+	<figcaption>Antall turer per ukedag som stolpediagram</figcaption>
+	<EnkeltStolpediagram overskrifter={['Ukedag', 'Turer']} data={ukedagtabell} />
+</figure>
