b41ea88b19a19c016594fb312e1df1e321b48773
Eirik Vågeskar <kode@cappelendamm.no>
2023-03-21 17:06:50 +0100

    Update explanation at front page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,16 +3,21 @@
 <nav>
 	<ul>
 		<li>
-			<a href="/utregning-i-nettleseren">Løsning med utregning i nettleseren.</a> Denne løsningen
-			bruker <em>kun</em> de delene av SvelteKit som Kode 2 lærer bort. Ulempen er at nettleseren er
-			nødt til å laste ned de 80 MB med data som ligger i <code>05.json</code> <em>hver gang</em> man
+			<a href="/utregning-i-nettleseren">Med utregning i nettleseren.</a> Denne løsningen bruker
+			<em>kun</em> de delene av SvelteKit som Kode 2 lærer bort. Ulempen er at nettleseren er nødt
+			til å laste ned de 80 MB med data som ligger i <code>05.json</code> <em>hver gang</em> man
 			gjør endringer i visningen.
+			<strong
+				>Dette gjør at man må vente i mer enn 10 sekunder hver gang man går inn på eller endrer
+				siden.</strong
+			>
 		</li>
 		<li>
-			<a href="/utregning-paa-tjeneren">Løsning med utregning på tjeneren.</a> Denne løsningen gjør
-			mesteparten av utregningene på tjeneren. Fordelen er at du slipper å vente like lenge når du
-			gjør endringer i <code>+page.svelte</code> (endringer i <code>+page.server.js</code> vil fortsatt
-			ta lang tid).
+			<a href="/utregning-paa-tjeneren">Med utregning på tjeneren.</a> Bruker teknikker fra
+			SvelteKit utenfor det som ble lært bort i Kode 2. Sparer data ved å regne ut resultatene på
+			tjeneren sende over ferdigutregnede tall til nettleseren. Lar være å gjøre de tunge
+			utregningene på nytt så lenge du ikke endrer på <code>+page.server.js</code>.
+			<strong>Dermed skal du få se endringene dine på skjermen på under ett sekund.</strong>
 		</li>
 	</ul>
 </nav>
